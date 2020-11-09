363170345848acc9bd7b927cab79d73bee943ef3
root <david.melanson20@gmail.com>
2020-11-09 21:29:45 +0000

    playerSelection

--- script.js
+++ script.js
@@ -17,29 +17,18 @@ function computerRandomNumber() {
 
 /*
 based on player and computer choice, alert YOU WIN or YOU LOSE
-    & computer chooses paper, alert YOU LOSE
-    & computer chooses scissor, alert YOU WIN
 */
 function playerChoseRock() {
+    playerSelection = "rock";
     alert("You chose rock! Great choice.")
 }
 
 function playerChosePaper() {
+    playerSelection = "paper";
     alert("You chose paper! Great choice.")
 }
 
 function playerChoseScissor() {
+    playerSelection = "scissor";
     alert("You chose scissor! Great choice.")
 }
-/*
-if the user chooses paper...
-    & computer chooses scissor, alert YOU LOSE
-    & computer chooses rock, alert YOU WIN
-*/
-
-
-/*
-if the user chooses scissor...
-    & computer chooses rock, alert YOU LOSE
-    & computer chooses paper, alert YOU WIN
-*/
