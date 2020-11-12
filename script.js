ab4259d0d9468e9c4a67b5c210464c2ef75647c7
root <david.melanson20@gmail.com>
2020-11-12 21:05:47 +0000

    buttons

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-/*  Link objects from HTML  */
+/*   Link objects from HTML   */
 const playerScore = document.querySelector('#playerScore');
 const computerScore = document.querySelector('#computerScore');
 const roundOutcome = document.querySelector('#roundOutcome');
 const gameOutcome = document.querySelector('#gameOutcome');
 
 
-/*
-Have the computer choose rock, paper or scissor at random
-*/
+/*   Have the computer choose rock, paper or scissor at random   */
 function computerPlay() {
     let randNumber = Math.random() * 3;
 
@@ -20,35 +18,23 @@ function computerPlay() {
     }
 }
 
-
-
-/*
-When the player clicks on a button, assign playerSelection and play game
-*/
-function playerChoseRock() {
-    computerPlay();
-    let playerSelection = 'rock';
+/*   function to play the game and give a result   */
+function playGame() {
     if (computerSelection == 'rock') {
         roundOutcome.innerHTML = 'ROUND TIE';
     } else if (computerSelection == 'paper') {
         roundOutcome.innerHTML = 'ROUND LOSS';
-        if (computerScore == 5) {
-            gameOutcome.innerHTML = 'Oh no, you lost to the computer.';
-        } else if (computerSelection == 4) {
-            computerScore.innerHTML ++;
-            gameOutcome.innerHTML = 'Oh no, you lost to the computer.';
-        } else {
-            computerScore.innerHTML ++;
-        }
+        computerScore.innerHTML ++;
     } else {
         roundOutcome.innerHTML = 'ROUND WIN';
-        if (playerScore == 5) {
-            gameOutcome.innerHTML = 'Congratulations, you won against the computer!';
-        } else if (playerScore == 4) {
-            playerScore.innerHTML ++;
-            gameOutcome.innerHTML = 'Congratulations, you won against the computer!';
-        } else {
-            playerScore.innerHTML ++;
-        }
+        playerScore.innerHTML ++;
     }
+}
+
+/*   When the player clicks on a button, 
+        assign playerSelection and play game   */
+function playerChoseRock() {
+    let playerSelection = 'rock';
+    computerPlay();
+    playGame();
 }
