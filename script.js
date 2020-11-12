f67af1cc3990ee9893479fdea530bad1ffd84d80
root <david.melanson20@gmail.com>
2020-11-12 14:09:02 +0000

    rock

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissor = document.querySelector('#scissor');
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
+const roundOutcome = document.querySelector('#roundOutcome');
+const gameOutcome = document.querySelector('#gameOutcome');
+const resetGame = document.querySelector('#resetGame');
+
+
 /*
 Have the computer choose rock, paper or scissor at random
 */
@@ -15,23 +25,32 @@ function computerPlay() {
     console.log(computerSelection);
 }
 
+
+/*
+Based on player and computer selection, return the outcome
+*/
+function playGame() {
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") {
+            roundOutcome.textContent("You both chose rock, it's a tie!");
+        } else if (computerSelection == "paper") {
+            roundOutcome.textContent("Paper beats rock, you lose!");
+        } else {
+            roundOutcome.textContent("Rock beats scissor, you win!");
+        }
+    }
+}
+
 /*
 When the player clicks on a button, assign playerSelection
 */
 function playerChoseRock() {
-    let playerSelection = "rock";
-    console.log(playerSelection);
+    computerPlay();
+    let playerSelection = "rock"
+    playGame();
 }
 
-function playerChosePaper() {
-    let playerSelection = "paper";
-    console.log(playerSelection);
-}
 
-function playerChoseScissor() {
-    let playerSelection = "scissor";
-    console.log(playerSelection);
-}
 
 /*
 Function to play the game
