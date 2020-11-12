0ec381f305832998c75295080e73367985ba2e69
root <david.melanson20@gmail.com>
2020-11-12 18:44:59 +0000

    try

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissor = document.querySelector('#scissor');
-const playerScore = document.querySelector('#player-Score');
-const computerScore = document.querySelector('#computer-Score');
-const roundOutcome = document.querySelector('#round-Outcome');
-const gameOutcome = document.querySelector('#game-Outcome');
-const resetGame = document.querySelector('#resetGame');
+/*  Link objects from HTML  */
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
+const roundOutcome = document.querySelector("roundOutcome");
+const gameOutcome = document.querySelector("gameOutcome");
 
-
-/*Set scores to zero to start*/
-function resetScore() {
-    let playerScore = 0;
-    document.getElementById("player-Score").value = playerScore;
-    let computerScore = 0;
-    document.getElementById("computer-Score").value = computerScore;
-}
-
-resetScore();
 
 /*
 Have the computer choose rock, paper or scissor at random
@@ -41,44 +28,10 @@ function playerChoseRock() {
     computerPlay();
     let playerSelection = "rock";
     if (computerSelection == "rock") {
-        document.getElementById("round-Outcome").innerHTML = "You both chose rock, it's a tie!";
-        return;
+        roundOutcome.innerHTML = "You tied";
     } else if (computerSelection == "paper") {
-        document.getElementById("round-Outcome").innerHTML = "Paper beats rock, you lose!";
-        document.getElementById("computer-Score").value = computerScore++;
-        return;
+        roundOutcome.innerHTML = "You lost";
     } else {
-        document.getElementById("round-Outcome").innerHTML = "Rock beats scissor, you win!";
-        document.getElementById("player-Score").value = playerScore++;
-        return;
+        roundOutcome.innerHTML = "You won!"
     }
 }
-
-function playerChosePaper() {
-    computerPlay();
-    let playerSelection = "paper";
-    if (computerSelection == "rock") {
-        document.getElementById("roundOutcome").innerHTML = "Paper beats rock, you win!";
-    } else if (computerSelection == "paper") {
-        document.getElementById("roundOutcome").innerHTML = "You both chose paper, it's a tie!";
-    } else {
-        document.getElementById("roundOutcome").innerHTML = "Scissor beats paper, you lose!";
-    }
-}
-
-function playerChoseScissor() {
-    computerPlay();
-    let playerSelection = "scissor";
-    if (computerSelection == "rock") {
-        document.getElementById("roundOutcome").innerHTML = "Rock beats scissor, you lose!";
-    } else if (computerSelection == "paper") {
-        document.getElementById("roundOutcome").innerHTML = "Scissor beats paper, you win!";
-    } else {
-        document.getElementById("roundOutcome").innerHTML = "You both chose scissor, it's a tie!";
-    }
-}
-
-
-/*
-Function to play the game
-*/
