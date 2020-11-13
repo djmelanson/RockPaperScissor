8c38ac978543dbf02d94b3aeb554f1050e8c8143
root <david.melanson20@gmail.com>
2020-11-13 13:58:20 +0000

    final

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const playerScore = document.querySelector('#playerScore');
 const computerScore = document.querySelector('#computerScore');
 const roundOutcome = document.querySelector('#roundOutcome');
-const gameOutcome = document.querySelector('#gameOutcome');
 const resetBtn = document.querySelector('#resetBtn');
 
 
@@ -29,24 +28,10 @@ function playerChoseRock() {
         roundOutcome.innerHTML = 'ROUND TIE';
     } else if (computerSelection == 'paper') {
         roundOutcome.innerHTML = 'ROUND LOSS';
-        if (computerScore == 5) {
-            gameOutcome.innerHTML = 'Oh no, you lost to the computer.';
-        } else if (computerScore == 4) {
-            computerScore.innerHTML ++;
-            gameOutcome.innerHTML = 'Oh no, you lost to the computer.';
-        } else {
-            computerScore.innerHTML ++;
-        }
+        computerScore.innerHTML ++;
     } else {
         roundOutcome.innerHTML = 'ROUND WIN';
-        if (playerScore == 5) {
-            gameOutcome.innerHTML = 'Congratulations, you beat the computer!';
-        } else if (playerScore == 4) {
-            playerScore.innerHTML ++;
-            gameOutcome.innerHTML = 'Congratulations, you beat the computer!';
-        } else {
-            playerScore.innerHTML ++;
-        }
+        playerScore.innerHTML ++;
     }
 }
         
